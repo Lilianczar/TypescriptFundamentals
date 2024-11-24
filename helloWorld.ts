b4481f4e142ed8a2a2349762df0f3520a632bcd4
lilianCzar <ubanekwol99@gmail.com>
2024-11-24 22:48:52 +1300

    first ts code

---
+++ helloWorld.ts
@@ -0,0 +1,3 @@
+console.log('Lilian Anyanka');
+console.log('Samczar is great');
+console.log("Serena Anyanka");
---
+++ variables.ts
@@ -0,0 +1,10 @@
+var myName = "Lilian";
+let myAge = 31
+const myGender ="Female"
+
+console.log("My name is "+myName+" And my age is "+myAge+" And i am a "+myGender );
+
+ myName = "Ada"
+ myAge = 30
+ console.log("My name is "+myName+" And my age is "+myAge+" And i am a "+myGender );
+ 
