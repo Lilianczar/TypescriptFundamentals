f895ee5a1dff2b41909d0281eb4252efbcf2b7ce
lilianCzar <ubanekwol99@gmail.com>
2024-11-25 00:00:31 +1300

    completion of dataType

--- dataType.ts
+++ dataType.ts
@@ -3,3 +3,35 @@
  const b = 2;
  
  console.log('is A Greater than B ? ', a > b)
+
+ let isDone:boolean = true;
+ let isEnable:boolean = false;
+
+ console.log(isDone);
+ console.log(isEnable);
+
+ //Number
+ let num1: number = 25
+ let num2: number = 99.99999
+
+ console.log(num1);
+ console.log(num2);
+
+ //String
+ let firstName: string = 'Lilian'
+ let lastName: string = 'Anyanka'
+
+ console.log(firstName);
+console.log(lastName);
+
+//Any
+let dynamicValue: any = 64
+console.log(dynamicValue);
+dynamicValue = 'Serena';
+console.log(dynamicValue);
+
+//Null and Undefined
+let nullValue: null = null;
+let undefinedValue: undefined = undefined;
+console.log(nullValue);
+console.log(undefinedValue); 
