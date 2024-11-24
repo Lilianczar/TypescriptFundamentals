2a743fbbd9306114d8c66b4122293a08411e8d84
SamCzar <senetor.samczar@gmail.com>
2024-11-24 23:10:32 +1300

    developed Calculator, remove helloworld

---
+++ examplesOfFunctions.ts
@@ -0,0 +1,5 @@
+function calculator(a:number, b:number){
+    return a + b;
+}
+
+console.log('2 + 3 is ', calculator(2,3))
--- helloWorld.ts
+++
@@ -1,3 +0,0 @@
-console.log('Lilian Anyanka');
-console.log('Samczar is great');
-console.log("Serena Anyanka");
